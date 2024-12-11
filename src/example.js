ac0d92b90f2146426821ccec55133f323769ba2a
maksimkaJCHK <maksimkajchk@rambler.ru>
2024-12-11 15:13:52 +0300

    Сделал единый обработчик ошибок, лучше обрабатываю ошибки

--- src/example.js
+++ src/example.js
@@ -1,6 +1,7 @@
 import { VK } from 'vk-io';
 
 import logger from './logger/logger.js';
+import errorHandling from './services/errorHandling.js';
 
 import getToken from './services/token.js';
 import { friends } from './data/data.js';
@@ -29,29 +30,36 @@ const vk = new VK({
 
 // Получаю друзей, подписки, подписчиков, группы
 const getTypeInfAboutUsers = async () => {
-  createFolders([
-    '../results',
-    '../results/user_friends'
-  ]);
+  if (friends && !friends.length) {
+    logger.error('Список пользователей пуст, не о ком собирать информацию.');
+  }
 
-  for (const item of friends) {
-    const { id, name } = item;
+  if (friends && friends.length) {
+    createFolders([
+      '../results',
+      '../results/user_friends'
+    ]);
+  
+    for (const item of friends) {
+      const { id, name } = item;
+  
+      logger.group(`Собираю информацию о пользователе - ${name}`);
+  
+      try {
+        const data = await getUserFriAndInt({ vk, id, name });
 
-    logger.group(`Собираю информацию о пользователе - ${name}`);
+        writeToJSON({
+          path: '../results/user_friends',
+          name: `${name.replace(/ /, '_')}-friend-${bDate()}`,
+          data,
+        });
+      } catch (error) {
+        errorHandling(error);
+        logger.error(`Не удалось собрать информацию о ${name}`);
+      }
 
-    try {
-      const data = await getUserFriAndInt({ vk, id, name });
-
-      writeToJSON({
-        path: '../results/user_friends',
-        name: `${name.replace(/ /, '_')}-friend-${bDate()}`,
-        data,
-      });
-    } catch (error) {
-      logger.error(`Не удалось собрать информацию о ${name}`);
+      logger.endGroup();
     }
-
-    logger.endGroup();
   }
 }
 
@@ -59,128 +67,140 @@ const getTypeInfAboutUsers = async () => {
 const getUsersInfoFromData = async () => {
   const savePath = '../results/example';
 
-  createFolders([
-    '../results',
-    savePath
-  ]);
-
   let friends = await readJSONFile({
     name: 'friends-parser',
     path: savePath
   });
 
-  const friendsIds = friends.map(({ id }) => id);
+  if (friends) {
+    createFolders([
+      '../results',
+      savePath
+    ]);
 
-  const getFriendInfo = async (ids) => {
-    const userFriends = await getUsersInfo(vk, ids);
+    const friendsIds = friends.map(({ id }) => id);
 
-    logger.success('Всего найдено, ' + userFriends.length);
+    const getFriendInfo = async (ids) => {
+      try {
+        const userFriends = await getUsersInfo(vk, ids);
 
-    writeToJSON({
-      path: savePath,
-      name: `friends-API-${bDate()}`,
-      data: userFriends,
-    });
+        logger.success('Всего найдено, ' + userFriends.length);
+
+        const nameSaveFile = `friends-API-${bDate()}`;
+
+        writeToJSON({
+          path: savePath,
+          name: nameSaveFile,
+          data: userFriends,
+        });
+
+        logger.success(`Файл "${savePath}/${nameSaveFile}" создан.`);
+      } catch (error) {
+        errorHandling(error);
+        logger.error(`Информацию о пользователе/ях:\n\n"${ids}"\n\nне удалось собрать.`)
+      }
+    }
+
+    await getFriendInfo(friendsIds.join(','));
+  } else {
+    logger.error(`Не удалось прочитать информацию из файла "${savePath}/friends-parser.json".`)
   }
-
-  await getFriendInfo(friendsIds.join(','));
 }
 
 const getFriendsCountUser = async () => {
-  let allProfiles = 0;
-  let openProfiles = 0;
-  let closeProfiles = 0;
   const savePath = '../results/example';
-
-  createFolders([
-    '../results',
-    savePath
-  ]);
+  const nameFile = 'friends-API-2024-12-10';
 
   let friends = await readJSONFile({
-    name: 'friends-API-2024-12-10',
+    name: nameFile,
     path: savePath
   });
 
-  for (const curFriend of friends) {
-    const { id, first_name, last_name } = curFriend;
+  if (friends && friends.length) {
+    createFolders([
+      '../results',
+      savePath
+    ]);
 
-    const name = `${first_name} ${last_name}`;
-    allProfiles++;
+    let allProfiles = 0;
+    let openProfiles = 0;
+    let closeProfiles = 0;
 
-    logger.group(`Сбор информации о пользователе ${name}, это ${allProfiles} пользователь`)
-
-    try {
-      const userFriends = await getUserFriends(vk, id, name);
-      const { items, count } = userFriends;
-
-      curFriend.friendsCount = count;
-      curFriend.friends = items;
-
-      openProfiles++;
-    } catch (error) {
-      if (error.code === 5) logger.error('Нужно авторизоваться, или срок действия токена закончился.');
-      if (error.code === 6) logger.error('Слишком много запросов в секунду.');
-      if (error.code === 10) logger.error('Произошла внутренняя ошибка сервера.');
-      if (error.code === 15) logger.error('Доступ запрещен.');
-      if (error.code === 18) logger.error(`Страница пользователя ${name} удалена, или заблокирована.`);
-      if (error.code === 28) logger.error('Ключ доступа устарел, или не действительный.');
-      if (error.code === 29) logger.error('Лимит на запросы исчерпан.');
-      if (error.code === 30) logger.error(`Профиль для ${name} закрыт.`);
-
-      logger.error(`Не удалось собрать информацию о пользователе ${name}, код ошибки ${error.code}.`);
-
-      curFriend.friendsCount = 0;
-      curFriend.friends = [];
-
-      closeProfiles++;
+    for (const curFriend of friends) {
+      const { id, first_name, last_name } = curFriend;
+  
+      const name = `${first_name} ${last_name}`;
+      allProfiles++;
+  
+      logger.group(`Сбор информации о пользователе ${name}, это ${allProfiles} пользователь`)
+  
+      try {
+        const userFriends = await getUserFriends(vk, id, name);
+        const { items, count } = userFriends;
+  
+        curFriend.friendsCount = count;
+        curFriend.friends = items;
+  
+        openProfiles++;
+      } catch (error) {
+        errorHandling(error)
+        logger.error(`Не удалось собрать информацию о пользователе ${name}, код ошибки ${error.code}.`);
+  
+        curFriend.friendsCount = 0;
+        curFriend.friends = [];
+  
+        closeProfiles++;
+      }
+  
+      logger.endGroup();
+      await delayF(500);
     }
-
-    logger.endGroup();
-    await delayF(500);
+  
+    // Я сортирую друзей по нормальному, а мне для моей задачи нужно от большего к меньшему, пока оставлю, а так тут заменить, и ничего переворачивать не нужно
+    friends = friends.sort((a, b) => {
+      if (a.friendsCount > b.friendsCount) {
+        return 1;
+      }
+  
+      if (a.friendsCount < b.friendsCount) {
+        return -1;
+      }
+  
+      return 0;
+    });
+  
+    logger.space();
+    logger.space();
+  
+    logger.success(`Всего обработано ${allProfiles} профилей`);
+    logger.success(`Открытых ${openProfiles} профилей`);
+    logger.success(`Закрытых ${closeProfiles} профилей`);
+  
+    const nameFile = `friend-API-full-sort-${bDate()}`;
+  
+    const paramsSaveFile = {
+      path: savePath,
+      name: nameFile,
+      spices: 0
+    }
+  
+    // Пока оставлю, но скорее всего мне это не понадобится
+    // writeToJSON({
+    //   data: newFriends,
+    //   ...paramsSaveFile
+    // });
+  
+    writeToJSON({
+      data: friends.reverse(),
+      ...paramsSaveFile
+    });
+  
+    logger.space();
+    logger.success(`Файл "${savePath}/${nameFile}" создан.`);
+  } else {
+    logger.error(`Не удалось прочитать информацию из файла "${savePath}/${nameFile}.json".`);
   }
-
-  // Я сортирую друзей по нормальному, а мне для моей задачи нужно от большего к меньшему, пока оставлю, а так тут заменить, и ничего переворачивать не нужно
-  friends = friends.sort((a, b) => {
-    if (a.friendsCount > b.friendsCount) {
-      return 1;
-    }
-
-    if (a.friendsCount < b.friendsCount) {
-      return -1;
-    }
-
-    return 0;
-  });
-
-  logger.space();
-  logger.space();
-
-  logger.success(`Всего обработано ${allProfiles} профилей`);
-  logger.success(`Открытых ${openProfiles} профилей`);
-  logger.success(`Закрытых ${closeProfiles} профилей`);
-
-  const nameFile = `friend-API-full-sort-${bDate()}`;
-
-  const paramsSaveFile = {
-    path: savePath,
-    name: nameFile,
-    spices: 0
-  }
-
-  // Пока оставлю, но скорее всего мне это не понадобится
-  // writeToJSON({
-  //   data: newFriends,
-  //   ...paramsSaveFile
-  // });
-
-  writeToJSON({
-    data: friends.reverse(),
-    ...paramsSaveFile
-  });
-
-  logger.space();
-  logger.success(`Файл "${savePath}/${nameFile}" создан.`);
 }
 
 getFriendsCountUser();
+//getUsersInfoFromData();
---
+++ src/services/errorHandling.js
@@ -0,0 +1,14 @@
+import logger from '../logger/logger.js';
+
+const errorHandling = ({ code }) => {
+  if (code === 5) logger.error('Нужно авторизоваться, или срок действия токена закончился.');
+  if (code === 6) logger.error('Слишком много запросов в секунду.');
+  if (code === 10) logger.error('Произошла внутренняя ошибка сервера.');
+  if (code === 15) logger.error('Доступ запрещен.');
+  if (code === 18) logger.error(`Страница пользователя ${name} удалена, или заблокирована.`);
+  if (code === 28) logger.error('Ключ доступа устарел, или не действительный.');
+  if (code === 29) logger.error('Лимит на запросы исчерпан.');
+  if (code === 30) logger.error(`Профиль для ${name} закрыт.`);
+}
+
+export default errorHandling;
