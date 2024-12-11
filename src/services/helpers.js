eba71f78ba3fe95a460bcea0ba4cc45fcfe3d11f
maksimkaJCHK <maksimkajchk@rambler.ru>
2024-12-11 17:41:16 +0300

    Начал делать функции для сравнения пользователей

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -1,6 +1,4 @@
-const bNumb = (numb) => {
-  return (numb < 10) ? '0' + numb : numb;
-}
+const bNumb = (numb) => (numb < 10) ? `0${numb}` : numb;
 
 export const bDate = () => {
   const date = new Date();
@@ -10,4 +8,41 @@ export const bDate = () => {
   const year = date.getFullYear();
 
   return `${year}-${month}-${day}`;
+}
+
+const bMapFromArr = (arr) => {
+  const bMap = new Map();
+
+  // Что-то должно быть ключом, в большинстве случаев это будет id-ик
+  arr.forEach((el) => bMap.set(el.id, el));
+
+  return bMap;
+}
+
+const compareArrObj = (fArr, sArr) => {
+  const commоn = [];
+  const fUniqVal = [];
+  const sUniqVal= [];
+
+  const fMap = bMapFromArr(fArr);
+  const sMap = bMapFromArr(sArr);
+
+  for (const id of fMap.keys()) {
+    const isItem = sMap.has(id);
+
+    if (isItem) {
+      commоn.push(fMap.get(id));
+      sMap.delete(id);
+    }
+
+    if (!isItem) fUniqVal.push(fMap.get(id));
+  }
+
+  for (const item of sMap.values()) sUniqVal.push(item);
+
+  return {
+    commоn,
+    fUniqVal,
+    sUniqVal
+  }
 }
